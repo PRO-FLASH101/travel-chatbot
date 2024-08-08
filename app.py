73016ad02ae477eb21c02b3a945118c28cecfbe9
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-08 15:55:57 +0530

    commit

--- app.py
+++ app.py
@@ -6,3 +6,4 @@ from langchain_community.llms import HuggingFaceEndpoint
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
+load_dotenv()  ## Load environment variables from .env file
